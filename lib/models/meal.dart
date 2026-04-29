e2c516c2f806d61362dc587e10cd0b9d35bffd06
Mussie <mussieworkneh@gmail.com>
2026-04-29 23:03:59 +0300

    added fromJson, toJson, and copyWith for models

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,5 +1,5 @@
 class Meal {
-  final int idMeal;
+  final String idMeal;
   final String strMeal;
   final String strMealThumb;
   final String strCategory;
@@ -18,4 +18,87 @@ class Meal {
     required this.strYoutube,
     required this.ingredients,
   });
+  
+  // map json data to Meal object filds
+  factory Meal.fromJson(Map<String, dynamic> json) {
+    final meal = Meal(
+      idMeal: json['idMeal'],
+      strMeal: json['strMeal'],
+      strMealThumb: json['strMealThumb'],
+      strCategory: json['strCategory'],
+      strArea: json['strArea'],
+      strInstructions: json['strInstructions'],
+      strYoutube: json['strYoutube'],
+      ingredients: [], // will be added later
+    );
+    
+    // adds an ingredients list to the meal object
+    return meal.copyWith(ingredients: meal._parseIngredients(json));
+  }
+  
+  // to convert individual key-value pairs of ingredients and measures to a list
+  List<String> _parseIngredients(Map<String, dynamic> json) {
+    final List<String> ingredientList = [];
+    
+    /**
+     * since ingredients are stored as strIngredient1, strIngredient2, ...20
+     * and each ingredients has measures as strMeasure1, strMeasure2, ...20
+     * we need to combine the ingredients and with their measures then 
+     * add the combined string to the list
+    */
+    
+    for(int i = 1; i <= 20; i++) {
+      final ingredient = json['strIngredient$i'];
+      final measure = json['strMeasure$i'];
+      
+      // check if the ingredient is null or an empty string
+      if(ingredient != null && ingredient.toString().trim().isNotEmpty) {
+        final combined = measure != null && // check if measure is null
+          measure.toString().trim().isNotEmpty // check if measure is empty string
+            ? '$measure $ingredient' // return as 1 kg Chicken
+            : ingredient; // if there is no measure
+        ingredientList.add(combined);
+      }
+    }
+    return ingredientList;
+  }
+  
+  // convert object data to json
+  Map<String, dynamic> toJson() {
+    return {
+      'idMeal': idMeal,
+      'strMeal': strMeal,
+      'strMealThumb': strMealThumb,
+      'strCategory': strCategory,
+      'strArea': strArea,
+      'strInstructions': strInstructions,
+      'strYoutube': strYoutube,
+      'ingredients': ingredients,
+    };
+  }
+  
+  // to change the values of filds of Meal object 
+  // or to copy object data to another
+  Meal copyWith({
+    String? idMeal,
+    String? strMeal,
+    String? strMealThumb,
+    String? strCategory,
+    String? strArea,
+    String? strInstructions,
+    String? strYoutube,
+    List<String>? ingredients, 
+  }) {
+    // change the initial value if only the values are provided
+    return Meal(
+      idMeal: idMeal ?? this.idMeal,
+      strMeal: strMeal ?? this.strMeal,
+      strMealThumb: strMealThumb ?? this.strMealThumb,
+      strCategory: strCategory ?? this.strCategory,
+      strArea: strArea ?? this.strArea,
+      strInstructions: strInstructions ?? this.strInstructions,
+      strYoutube: strYoutube ?? this.strYoutube,
+      ingredients: ingredients ?? this.ingredients,
+    );
+  }
 }
--- lib/models/meal_category.dart
+++ lib/models/meal_category.dart
@@ -1,5 +1,5 @@
 class MealCategory {
-  final int idCategory;
+  final String idCategory;
   final String strCategory;
   final String strCategoryThumb;
   final String strCategoryDescription;
@@ -10,4 +10,42 @@ class MealCategory {
     required this.strCategoryThumb,
     required this.strCategoryDescription,
   });
+  
+  // map json data to MealCategory object filds
+  factory MealCategory.fromJson(Map<String, dynamic> json) {
+    return MealCategory(
+      idCategory: json['idCategory'],
+      strCategory: json['strCategory'],
+      strCategoryThumb: json['strCategoryThumb'],
+      strCategoryDescription: json['strCategoryDescription'],
+    );
+  }
+  
+  // convert object data to json
+  Map<String, dynamic> tojson() {
+    return {
+      'idCategory': idCategory,
+      'strCategory': strCategory,
+      'strCategoryThumb': strCategoryThumb,
+      'strCategoryDescription': strCategoryDescription,
+    };
+  }
+  
+  // to change the values of filds of MealCategory object
+  // or to copy object data to another
+  MealCategory copyWith({
+    String? idCategory,
+    String? strCategory,
+    String? strCategoryThumb,
+    String? strCategoryDescription,
+  }) {
+    // change the initial value if only the values are provided
+    return MealCategory(
+      idCategory: idCategory ?? this.idCategory,
+      strCategory: strCategory ?? this.strCategory,
+      strCategoryThumb: strCategoryThumb ?? this.strCategoryThumb,
+      strCategoryDescription: 
+        strCategoryDescription ?? this.strCategoryDescription,
+    );
+  }
 }
